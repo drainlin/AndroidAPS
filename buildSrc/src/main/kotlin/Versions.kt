ef1299a9c3e2138ab97e4043e738cf66a67ac486
Yulin <Drainlin@iCloud.com>
2025-11-09 10:53:33 +0800

    更新了移宇和app设置的中文翻译

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object Versions {
 
     const val appVersion = "3.3.2.1"
-    const val versionCode = 1500
+    const val versionCode = 1501
 
     const val ndkVersion = "21.1.6352462"
 
